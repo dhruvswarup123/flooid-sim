f271da08aef4aa7aca30b4b16c47d38f66a2ade6
Dhruv Swarup <dhruvswarup123@gmail.com>
2023-10-11 19:40:17 -0700

    Organize into an app

--- src/app.cpp
+++ src/app.cpp
@@ -5,9 +5,106 @@ void App::setup() {
 }
 
 void App::loop() {
-    glBegin(GL_TRIANGLES);
-    glVertex2f(-0.5, -0.5);
-    glVertex2f(0, 0.5);
-    glVertex2f(0.5, -0.5);
-    glEnd();
+    glfwPollEvents();
+
+    // Start the Dear ImGui frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    {
+        static float f = 0.0f;
+        static int counter = 0;
+
+        ImGui::Begin("Hello, world!");                          // Create a window called "Hello, world!" and append into it.
+
+        ImGui::Text("This is some useful text.");               // Display some text (you can use a format strings too)
+
+        ImGui::SliderFloat("float", &f, 0.0f, 1.0f);            // Edit 1 float using a slider from 0.0f to 1.0f
+
+        if (ImGui::Button("Button"))                            // Buttons return true when clicked (most widgets return true when edited/activated)
+            counter++;
+        ImGui::SameLine();
+        ImGui::Text("counter = %d", counter);
+
+        ImGui::Text("Application average %.3f ms/frame (%.1f FPS)", 1000.0f / io_->Framerate, io_->Framerate);
+        ImGui::End();
+    }
+
+    // Rendering
+    ImGui::Render();
+    int display_w, display_h;
+    glfwGetFramebufferSize(window_, &display_w, &display_h);
+    glViewport(0, 0, display_w, display_h);
+    glClearColor(kClearColor.x * kClearColor.w, kClearColor.y * kClearColor.w, kClearColor.z * kClearColor.w, kClearColor.w);
+    glClear(GL_COLOR_BUFFER_BIT);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    glfwSwapBuffers(window_);
+}
+
+bool App::init() {
+    glfwSetErrorCallback(glfwErrorCallback);
+    if (!glfwInit()) return false;
+
+    // Decide GL+GLSL versions
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+    // GL ES 2.0 + GLSL 100
+    const char* glsl_version = "#version 100";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 2);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    glfwWindowHint(GLFW_CLIENT_API, GLFW_OPENGL_ES_API);
+#elif defined(__APPLE__)
+    // GL 3.2 + GLSL 150
+    const char* glsl_version = "#version 150";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 2);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // Required on Mac
+#else
+    // GL 3.0 + GLSL 130
+    const char* glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+    //glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);  // 3.2+ only
+    //glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);            // 3.0+ only
+#endif
+
+    // Create window with graphics context
+    window_ = glfwCreateWindow(1280, 720, "This is a window", nullptr, nullptr);
+    if (window_ == nullptr) return false;
+    glfwMakeContextCurrent(window_);
+    glewInit();
+    glfwSwapInterval(0);
+
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    io_ = &ImGui::GetIO(); 
+    io_->ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;     // Enable Keyboard Controls
+
+    // Setup Dear ImGui style
+    ImGui::StyleColorsDark();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window_, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    // Do app setup here
+    setup();
+
+    return true;
+}
+
+bool App::windowShouldClose() {
+    return glfwWindowShouldClose(window_);
+}
+
+void App::deinit() {
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window_);
+    glfwTerminate();
 }
--- src/app.h
+++ src/app.h
@@ -1,13 +1,50 @@
 #pragma once
+
+// C++ Libs
+#include <stdio.h>
 #include <windows.h>
+#include <optional>
+
+// OpenGL/GLEW -- must be before other gl imports
+#define GLEW_STATIC
+#include <GL/glew.h>
+
+// Imgui
+#include <imgui.h>
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+
+// GLFW
+#define GL_SILENCE_DEPRECATION
+#if defined(IMGUI_IMPL_OPENGL_ES2)
+#include <GLES2/gl2.h>
+#endif
+#include <GLFW/glfw3.h>
+
 #include <GL/gl.h>  // GLUT, includes glu.h and gl.h
 
 
 class App final {
 public:
+	bool init();
+
 	void setup();
 
+	bool windowShouldClose();
+
 	void loop();
+
+	void deinit();
 private:
+	static void glfwErrorCallback(int error, const char* description) {
+		fprintf(stderr, "GLFW Error %d: %s\n", error, description);
+	}
+	
+	// Constants
+	static constexpr ImVec4 kClearColor{ 25/255.0f, 65/255.0f, 105/255.0f, 1.00f };
+
+	// Setup variables
+	GLFWwindow* window_{};
+	ImGuiIO* io_{};
 };
 
